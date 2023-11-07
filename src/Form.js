eb7905c2faacb37c944e4613798eef51f3525839
Pankaj yadav <abhi7868090@gmail.com>
2023-11-07 23:53:13 +0530

    ctrate form

--- src/Form.js
+++ src/Form.js
@@ -2,15 +2,13 @@ import React from 'react'
 
 const Form = () => {
   return (
-    <div className='container'>
-    <form>
+    <div className="container-fluid ">
+    <form className="bg-black text-white ">
         <label>First Name</label>
         <input type='text' placeholder='Enter your name' />
         <label>Mobile</label>
         <input type='number' placeholder='Enter your name' />
-        <label>Date</label>
-        <input type='date' placeholder='Enter your name' />
-        <button className='btn btn-success'>Add</button>
+        <button className='btn btn-success'>Add</button> 
     </form>
     </div>
   )
